f82952e50ab4026ed2bade6935a5577ae9cc3b1a
aminahesham2010-art <aminahesham2010@gmail.com>
2026-01-11 18:18:56 +0200

    phone updates

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function changeCategory(id){
   document.getElementsByClassName("alc")[0].style.display="none";
     document.getElementsByClassName("soft")[0].style.display="none";
       document.getElementsByClassName("noalc")[0].style.display="none";
-        //document.getElementsByClassName("hot")[0].style.display="none";
+        document.getElementsByClassName("hot")[0].style.display="none";
 
 if(id =="cck")
  {
@@ -21,6 +21,10 @@ if(id =="soft")
     if(id =="noalc")
  {
         document.getElementsByClassName("noalc")[0].style.display="block";
+    }
+        if(id =="hot")
+ {
+        document.getElementsByClassName("hot")[0].style.display="block";
     }
 document.getElementById(id).classList.add("active");
     }
@@ -44,4 +48,14 @@ document.getElementById(idevent).style.display="flex";
     }
 
 
-    
+    function show_current_cck(number){
+        var cocktailz=document.getElementById("cocktailz");
+        console.log(cocktailz);
+        cocktailz.children[number].style.animation="showCocktail  1.5s ease-in forwards";
+       setTimeout(() => {
+           cocktailz.children[number].style.animation="";
+               cocktailz.children[number].style.opacity="0"; 
+       }, 3000);
+
+
+    }
